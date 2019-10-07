24c7479ee4a3ab3da48da405cea6a3284ef13234
MichielsNoella <michiels.noella@skynet.be>
2019-10-07 09:50:11 +0200

    use of database

--- src/app/shopping/shopping-list/shopping-list.component.ts
+++ src/app/shopping/shopping-list/shopping-list.component.ts
@@ -1,5 +1,4 @@
 import {Component, OnInit} from '@angular/core';
-import {Observable} from 'rxjs';
 import {Shopping} from '../shopping.model';
 import {StaticDataSource} from '../static.datasource';
 
@@ -9,21 +8,53 @@ import {StaticDataSource} from '../static.datasource';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-
-  shoppings: Shopping[];
+  toDoListArray: any[];
+  // shoppings: Shopping[];
 
   constructor(
     private service: StaticDataSource
   ) {
   }
 
+  // ngOnInit() {
+  //   this.service.getShopping().subscribe(res => this.shoppings = res);
+  //   // this.service.getShopping().subscribe(res => this.shoppings = res.filter(item => !item.done));
+  // }
+  //
+  // getTodoItems() {
+  //   // return this.shoppings;
+  //   return this.shoppings.filter(item => !item.done);
+  //   // return this.model.items; // geeft de waarde terug
+  // }
+
   ngOnInit() {
-    this.service.getShopping().subscribe(res => this.shoppings = res);
-    // this.service.getShopping().subscribe(res => this.shoppings = res.filter(item => !item.done));
+    this.service.getToDoList().snapshotChanges()
+      .subscribe(item => {
+        this.toDoListArray = [];
+        item.forEach(element => {
+          const x = element.payload.toJSON();
+          x['$key'] = element.key;
+          this.toDoListArray.push(x);
+        });
+
+        // sort array isChecked false  -> true
+        this.toDoListArray.sort((a, b) => {
+          return a.isChecked - b.isChecked;
+        });
+      });
   }
-  getTodoItems() {
-    // return this.shoppings;
-    return this.shoppings.filter(item => !item.done);
-    // return this.model.items; // geeft de waarde terug
+
+  onAdd(itemTitle, itemAmount) {
+    this.service.addTitle(itemTitle.value, itemAmount.value);
+    itemTitle.value = null;
+    itemAmount.vale = null;
+  }
+
+  alterCheck($key: string, isChecked) {
+    this.service.checkOrUnCheckTitle($key, !isChecked);
+  }
+
+  onDelete($key: string) {
+    this.service.removeTitle($key);
   }
 }
--- src/app/shopping/static.datasource.ts
+++ src/app/shopping/static.datasource.ts
@@ -1,29 +1,55 @@
 import {Injectable} from '@angular/core';
-import {from, Observable} from 'rxjs';
-import {Shopping} from './shopping.model';
 import {root} from 'rxjs/internal-compatibility';
-
-import {filter, map} from 'rxjs/operators';
+import {AngularFireDatabase, AngularFireList} from 'angularfire2/database';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StaticDataSource {
-  private shopping: Shopping[] = [
-    new Shopping(1, 'AA', 25, false),
-    new Shopping(2, 'AB', 75, false),
-    new Shopping(2, 'AB', 75, true)
-  ];
+  toDoList: AngularFireList<any>;
 
-  getShopping(): Observable<Shopping[]> {
-    return from([this.shopping]);
-    // return from([this.shopping]).pipe(
-    //   map(res => res.filter(p => !p.done))
-    // );
+  constructor(private firebasedb: AngularFireDatabase) {
   }
 
-  getShoppingd(): Observable<Shopping[]> {
-    return from([this.shopping]);
+
+  // private shopping: Shopping[] = [
+  //   new Shopping(1, 'AA', 25, false),
+  //   new Shopping(2, 'AB', 75, false),
+  //   new Shopping(2, 'AB', 75, true)
+  // ];
+
+  // getShopping(): Observable<Shopping[]> {
+  //   return from([this.shopping]);
+  //   // return from([this.shopping]).pipe(
+  //   //   map(res => res.filter(p => !p.done))
+  //   // );
+  // }
+
+  // getShoppingd(): Observable<Shopping[]> {
+  //   return from([this.shopping]);
+  // }
+
+  getToDoList() {
+    this.toDoList = this.firebasedb.list('shoppings');
+    return this.toDoList;
+  }
+
+  addTitle(shopping: string, amount: number) {
+    this.toDoList.push({
+      // title: title,
+      isChecked: false,
+      shoppingName: shopping,
+      amounts: amount
+
+    });
+  }
+
+  checkOrUnCheckTitle($key: string, flag: boolean) {
+    this.toDoList.update($key, {isChecked: flag});
+  }
+
+  removeTitle($key: string) {
+    this.toDoList.remove($key);
   }
 
   //
